139e77f961813dc51e93e7b179c836d8e21c1d86
Billy Charlton <billy@okbecause.com>
2022-11-17 16:47:44 +0100

    got prettier working

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,23 @@
-// eslintrc.js
-{
-  extends: [
-  'eslint:recommended',
-  "plugin:vue/vue3-recommended",
-  "prettier"
-],
-}
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential', '@vue/typescript'],
+  rules: {
+    'no-console': 'off', // process.env.NODE_ENV === "production" ? "error" : "off",
+    'no-debugger': 'off', // process.env.NODE_ENV === "production" ? "error" : "off",
+    'no-var': 'off',
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
